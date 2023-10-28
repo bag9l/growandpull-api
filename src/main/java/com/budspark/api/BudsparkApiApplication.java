996693ee0e3a60d6eeef29ab5ca6bb24e96e9ee9
bag9l <vadimlyashok03@gmail.com>
2023-10-28 14:32:49 +0300

    init

---
+++ src/main/java/com/budspark/api/BudsparkApiApplication.java
@@ -0,0 +1,13 @@
+package com.budspark.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudsparkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudsparkApiApplication.class, args);
+	}
+
+}
