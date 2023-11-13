abf3b26557bee029e3b625ea1c3fa2a9a752fe98
liza <elizabeth1204jjj@gmail.com>
2023-11-13 19:15:14 +0200

    changes in controller

--- src/main/java/com/growandpull/api/controller/AuthenticationController.java
+++ src/main/java/com/growandpull/api/controller/AuthenticationController.java
@@ -1,9 +1,6 @@
 package com.growandpull.api.controller;
 
-import com.growandpull.api.dto.ProfileView;
-import com.growandpull.api.dto.StartupUpdateRequest;
-import com.growandpull.api.dto.StartupView;
-import com.growandpull.api.dto.UserUpdateRequest;
+import com.growandpull.api.dto.*;
 import com.growandpull.api.dto.auth.AuthenticatedUser;
 import com.growandpull.api.dto.auth.AuthenticationRequest;
 import com.growandpull.api.dto.auth.AuthenticationResponse;
@@ -23,6 +20,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @RestController
@@ -77,5 +76,35 @@ public class AuthenticationController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+    @PutMapping("updatePassword/{userId}")
+    public ResponseEntity<AuthenticationResponse> updatePassword(
+            @Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest,
+            @AuthenticationPrincipal UserDetails userDetails, @PathVariable String userId) throws java.io.IOException {
+        try {
+            if (!userId.equals(userDetails.getUsername())) {
+                throw new PermissionException("You do not have permission to update this password");
+            }
+            AuthenticationResponse updatedAuthentication = userService.updatePassword(userDetails, passwordUpdateRequest);
+            return ResponseEntity.ok(updatedAuthentication);
+        } catch (PermissionException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("profile/{userId}")
+    public ResponseEntity<?> getProfile(@PathVariable String userId,
+                                        @AuthenticationPrincipal UserDetails userDetails,
+                                        PublicProfile publicProfile,
+                                        PrivateProfile privateProfile) {
+        if (Objects.equals(userId, userDetails.getUsername())) {
+            return ResponseEntity.ok(privateProfile);
+        } else {
+            return ResponseEntity.ok(publicProfile);
+        }
+    }
+
 
 }
