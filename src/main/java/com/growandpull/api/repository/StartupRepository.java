82c8ae369b199bca210c12b6c1b4ac2ba2c4b90a
bag9l <vadimlyashok03@gmail.com>
2023-12-12 12:34:17 +0200

    fix repository methods containing date param

--- src/main/java/com/growandpull/api/repository/StartupRepository.java
+++ src/main/java/com/growandpull/api/repository/StartupRepository.java
@@ -2,6 +2,13 @@ package com.growandpull.api.repository;
 
 import com.growandpull.api.model.entity.Startup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface StartupRepository extends JpaRepository<Startup, String> {
+
+    List<Startup> findAllByOwnerEmailAndCreatedAtAfter(@Param("email") String email,
+                                                       @Param("threeMonthsAgo") LocalDateTime threeMonthsAgo);
 }
